36a3c873c80525072539d17e2d03d12e4e59c4a8
zhiyuan_zhu <zhiyuan_zhu@htc.com>
2017-09-07 21:50:27 +0800

    add doc

---
+++ memory_study/mem_main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+static char line[4096];
+
+int main() {
+	printf("memory study main...\n");
+
+	char *malloc_ptr = malloc(1024*1024);
+	FILE *fh = NULL;
+
+	fh = fopen("/proc/ft2", "w");
+
+	sprintf(line, "pid: %d malloc_addr: %p\n", getpid(), malloc_ptr);
+
+	fputs(line, fh);
+	fclose(fh);
+
+	printf("line: %s\n", line);
+	
+	printf("a=%p\n", malloc_ptr);
+	
+	while(1) {
+		sleep(2);
+	}
+
+	free(malloc_ptr);
+	fclose(fh);
+	return 0;
+}
